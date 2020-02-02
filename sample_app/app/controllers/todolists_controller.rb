2dc153170dc7e6b90e95bd7f66f0c0909b9f5058
oowada takuji <jiinyo829@gmail.com>
2020-02-02 13:32:38 +0900

    3-6[finish]

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -3,14 +3,21 @@ class TodolistsController < ApplicationController
   	@list = List.new
   end
 
-  def create
-        # ストロングパラメーターを使用
+	def create
          list = List.new(list_params)
-        # DBへ保存する
          list.save
-        # トップ画面へリダイレクト
-        redirect_to '/top'
+         redirect_to todolist_path(list.id)
     end
+
+    def index
+    	@lists = List.all
+    end
+
+    def show
+    	@list = List.find(params[:id])
+    end
+
+
     private
 
     def list_params
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   post 'todolists' => 'todolists#create'
 
-  get'todolist'=>'todolist#index'
+  get'todolists'=>'todolists#index'
+
+  get'todolists/:id'=>'todolists#show',as:'todolist'
 end
