4419a3f91fe68d64f52ebb7999bc9c2b79e7eaad
oowada takuji <jiinyo829@gmail.com>
2020-02-02 15:25:30 +0900

    4-1[start]

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get'todolists/:id/edit'=>'todolists#edit',as:'edit_todolist'
 
-  patch'todolists/:id'=>'todolists#update',as:'update_stodolist'
+  patch'todolists/:id'=>'todolists#update',as:'update_todolist'
 
   delete'todolists/:id'=>'todolists#destroy',as:'destroy_todolist'
 
