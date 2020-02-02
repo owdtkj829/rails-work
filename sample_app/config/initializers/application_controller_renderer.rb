7e6646e6a5162e067f85758e153df3050b874e2b
oowada takuji <jiinyo829@gmail.com>
2020-02-02 14:36:07 +0900

    3-8[finish]

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1b27a78d863b1f44164fb406227e32bb3b8d2f4800ce5d18b9ac58b0ecce31bfb7a2fe0480074d8851ec9b1098ff5ff83f03a7187f46bee77e29d88b6bd4ae55'
