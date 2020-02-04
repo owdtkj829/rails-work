884e77ca52e971c28388b674a0675bb810f2453e
oowada takuji <jiinyo829@gmail.com>
2020-02-04 16:25:37 +0900

    5-3[start]

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b32854cab58544738c332408af49409055b42d7224a611a3e40f7a2a8d715cf1fa3414ace94dbab59db6c287a2b51e2041184ca253ed63aca15fca0be1d27ca2'
