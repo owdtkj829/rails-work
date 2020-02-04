1a8af87378fdaf352e5e58a472c96c2cadc1698f
oowada takuji <jiinyo829@gmail.com>
2020-02-04 13:29:55 +0900

    5-2[finish]

--- meshiterro/app/controllers/application_controller.rb
+++ meshiterro/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :device_controller?
+	protected
+	def configure_permitted_parameters
+		device_parameter_sanitizer.permit(:sign_up,keys:[:name])
+	end
 end
