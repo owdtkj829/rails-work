206f59f8257009fa3379622d46dbf2e59fd9e99f
oowada takuji <jiinyo829@gmail.com>
2020-02-08 17:16:46 +0900

    5-9[課題作成途中]

--- vagrant/bookers/app/controllers/application_controller.rb
+++ vagrant/bookers/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    user_path(resouce.id)
   end
   def after_sign_out_path_for(resource)
-    new_user_session_path
+    root_path
   end
 end
